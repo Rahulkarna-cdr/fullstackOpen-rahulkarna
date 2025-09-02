9fc2fb0110a80c9772affb07c35d18f4d73f62fb
Rahul Karna <rahulkarna889@gmail.com>
2025-09-02 20:34:11 +0545

    exercise 3.2 completed

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,32 +1,41 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 
-let data = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+const data =[
+  { 
+    "id": "1",
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": "2",
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": "3",
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": "4",
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
 ]
-const app = http.createServer((request,response)=>{
-    response.writeHead(200,{'Content-Type':'application/json'})
-    response.end(JSON.stringify(data))
+
+app.get('/', (request,response)=>{
+  response.send(data)
+})
+
+app.get('/info', (request,response)=>{
+  const date = new Date()
+  response.send(`
+  <p>phonebook has info for ${data.length} people </p>
+  <p>${date}</p>
+  `)
 })
 
 const port = 3001
 app.listen(port)
-console.log(`the server is running on port ${port}`)
+console.log(`server has started at port ${port}`)
