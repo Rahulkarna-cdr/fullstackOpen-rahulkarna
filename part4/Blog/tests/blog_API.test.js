cb7967fdb70ddb9e59b4554f29cf40fabcdb816e
rahul karna <rahulkarna889@gmail.com>
2025-09-24 10:49:52 +0545

    Exercise 4.8 completed

---
+++ part4/Blog/tests/blog_API.test.js
@@ -0,0 +1,23 @@
+const { test, after } = require('node:test')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const assert = require('node:assert')
+
+const api = supertest(app)
+
+test.only('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test.only("All blogs are returned", async ()=>{
+    const res = await api.get('/api/blogs')
+
+    assert.strictEqual(res.body.length, 2)
+})
+after(async () => {
+  await mongoose.connection.close()
+})
