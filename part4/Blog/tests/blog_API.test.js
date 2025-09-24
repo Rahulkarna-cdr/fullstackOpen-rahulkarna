f324e33f7a8e499efb51340d20b0a14e04f61ecd
rahul karna <rahulkarna889@gmail.com>
2025-09-24 11:17:12 +0545

    Exercise 4.9 completed

--- part4/Blog/tests/blog_API.test.js
+++ part4/Blog/tests/blog_API.test.js
@@ -18,6 +18,21 @@ test.only("All blogs are returned", async ()=>{
 
     assert.strictEqual(res.body.length, 2)
 })
+
+test.only('unique identifier property of the blog posts is named id', async()=>{
+    const response = await api.get('/api/blogs')
+/* In 'response' we have something like this
+{
+  statusCode: 200,
+  headers: { ... },
+  body: [ { id: 'abc123', title: 'Blog 1' }, ... ],
+  text: '[{"id":"abc123","title":"Blog 1"}]',
+  ...
+}
+  */
+    const blogs = response.body
+   assert.ok(blogs[0].id, "id property is missing");
+})
 after(async () => {
   await mongoose.connection.close()
 })
