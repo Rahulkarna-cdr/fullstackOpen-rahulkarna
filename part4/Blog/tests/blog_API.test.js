b3e7dde95f8f47f10de927abc5bd9cc171e48975
rahul karna <rahulkarna889@gmail.com>
2025-09-24 12:45:24 +0545

    Exercise 4.10 completed

--- part4/Blog/tests/blog_API.test.js
+++ part4/Blog/tests/blog_API.test.js
@@ -1,10 +1,47 @@
-const { test, after } = require('node:test')
+const { test, after, beforeEach } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const assert = require('node:assert')
+const Blog = require('../models/blog')
 
 const api = supertest(app)
+
+const initialBlogs = [
+  {
+    title: 'Software Architecture: The Hard Parts',
+    author: 'Neal Ford',
+    url: 'www.nealFord.com',
+    likes: 54,
+  },
+  {
+    title: 'Clean Code',
+    author: 'Robert C. Martin',
+    url: 'www.cleancode.com',
+    likes: 75,
+  },
+  {
+    title: 'Design Patterns',
+    author: 'Erich Gamma',
+    url: 'www.designpatterns.com',
+    likes: 100,
+  }
+]
+
+
+beforeEach(async ()=>{
+    await Blog.deleteMany({})
+
+    let blogObj = new Blog(initialBlogs[0])
+    await blogObj.save()
+
+    blogObj = new Blog(initialBlogs[1])
+    await blogObj.save()
+
+    blogObj = new Blog(initialBlogs[2])
+    await blogObj.save()
+})
+
 
 test.only('blogs are returned as json', async () => {
   await api
@@ -16,7 +53,7 @@ test.only('blogs are returned as json', async () => {
 test.only("All blogs are returned", async ()=>{
     const res = await api.get('/api/blogs')
 
-    assert.strictEqual(res.body.length, 2)
+    assert.strictEqual(res.body.length, 3)
 })
 
 test.only('unique identifier property of the blog posts is named id', async()=>{
@@ -32,6 +69,25 @@ test.only('unique identifier property of the blog posts is named id', async()=>{
   */
     const blogs = response.body
    assert.ok(blogs[0].id, "id property is missing");
+})
+
+test.only("blogs are successfully created..", async()=>{
+    const newBlog =
+        {
+            title: 'Journey to the center of the Earth',
+            author: 'Martin damsel',
+            url: 'www.centerearth.com',
+            likes: 46
+        }
+
+    const response = await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type',/application\/json/)
+
+     const anotherResponse = await api.get('/api/blogs')
+    assert.strictEqual(anotherResponse.body.length, initialBlogs.length+1)
 })
 after(async () => {
   await mongoose.connection.close()
