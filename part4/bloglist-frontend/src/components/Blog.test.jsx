8a6dd8beba620c9e9387920cad9146f9c52a12ca
rahul karna <rahulkarna889@gmail.com>
2025-11-04 12:48:15 +0545

    Exercise 5.14 Completed

--- part4/bloglist-frontend/src/components/Blog.test.jsx
+++ part4/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import Blog from "./Blog";
 import { expect } from "vitest";
+import userEvent from "@testing-library/user-event";
 
 test("renders title but not details by default", () => {
   const blog = {
@@ -23,3 +24,35 @@ test("renders title but not details by default", () => {
   const urlElement = screen.queryByText("https://example.com/guide-to-mongodb");
   expect(urlElement).toBeNull();
 });
+
+describe("blogs URL and number of likes are shown when the button controlling the shown details has been clicked.", () => {
+  const blog = {
+    title: "testing content",
+    author: "Test",
+    url: "https://example.com/guide-to-mongodb",
+    likes: 6,
+    user: {
+      username: "test_User",
+      name: "test",
+      blogs: [],
+    },
+  };
+
+  beforeEach(() => {
+    render(<Blog blog={blog} setBlogs={() => {}} />);
+  });
+
+  test("at start blogs URL and number of likes are not displayed", () => {
+    expect(screen.queryByText(blog.url)).toBeNull();
+    expect(screen.queryByText(/Likes:/)).toBeNull();
+  });
+
+  test('after clicking, blogs URL and number of likes are displayed', async () => {
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+    expect(screen.getByText(blog.url, { exact: false })).toBeVisible()
+    expect(screen.getByText(/Likes: 6/)).toBeVisible()
+  });
+});
+
