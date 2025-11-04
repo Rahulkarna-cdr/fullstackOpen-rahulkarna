a59f3c73220d6851c0931bf2b8c8b606e95b4bc2
rahul karna <rahulkarna889@gmail.com>
2025-11-04 15:14:59 +0545

    Exercise 5.17 Completed

---
+++ part4/playwright/tests/blog.spec.js
@@ -0,0 +1,18 @@
+const { test, expect, beforeEach, describe } = require('@playwright/test')
+
+describe('Blog app', () => {
+  beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:5173')
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    await page.goto('http://localhost:5173')
+    
+    const locator = page.getByText('log in to application')
+    await expect(locator).toBeVisible()
+    await expect(page.getByLabel('username')).toBeVisible()
+    await expect(page.getByLabel('password')).toBeVisible()
+    await expect(page.getByRole('button',{name:'login'})).toBeVisible()
+
+  })
+})
