a8fc22aa3bb78acc94de248ba02938ea7e74dced
rahul karna <rahulkarna889@gmail.com>
2025-11-04 23:38:36 +0545

    Exercise6.2 Completed

--- part6/unicafe-redux/src/main.jsx
+++ part6/unicafe-redux/src/main.jsx
@@ -11,6 +11,7 @@ const App = () => {
       <button onClick={() => store.dispatch({ type: 'OK' })}>ok</button>
       <button onClick={() => store.dispatch({ type: 'BAD' })}>bad</button>
       <button onClick={() => store.dispatch({ type: 'RESET' })}>reset stats</button>
+      
       <div>good {store.getState().good}</div>
       <div>ok {store.getState().ok}</div>
       <div>bad {store.getState().bad}</div>
