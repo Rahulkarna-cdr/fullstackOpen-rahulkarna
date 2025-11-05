8f9932bfb48b51bd5a1312cfaa04898bbb006cc4
rahul karna <rahulkarna889@gmail.com>
2025-11-05 21:03:33 +0545

    Exercise 6.8 Completed

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,39 +1,11 @@
-import { useDispatch, useSelector } from "react-redux";
-import { createAnecdote, voteIncrement } from "./reducers/anecdoteReducer";
 import AnecdoteForm from "./components/AnecdoteForm";
+import AnecdoteList from "./components/AnecdoteList";
 
 const App = () => {
-  const anecdotes = useSelector((state) => 
-  [...state].sort((a,b)=>b.votes - a.votes)
-  );
-
-  const dispatch = useDispatch();
-
-  const vote = (id) => {
-    dispatch(voteIncrement(id));
-  };
-
-  const handleCreateAnecdote = (e) => {
-    e.preventDefault();
-    const content = e.target.anecdote.value;
-    e.target.anecdote.value = "";
-    dispatch(createAnecdote(content));
-  };
-
   return (
     <div>
-      <h2>Anecdotes</h2>
-      {anecdotes.map((anecdote) => (
-        <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      ))}
-      <AnecdoteForm handleCreateAnecdote={handleCreateAnecdote} />
-      
+     <AnecdoteList />
+      <AnecdoteForm />      
     </div>
   );
 };
