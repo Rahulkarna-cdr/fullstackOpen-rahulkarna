0e72e6dd1394dff623656da2f7e2e59c7d1c2a5c
rahul karna <rahulkarna889@gmail.com>
2025-11-11 11:59:58 +0545

    Exercise 7.4 Completed

---
+++ part7/routed-anecdotes/src/hooks/index.js
@@ -0,0 +1,18 @@
+//custom hooks
+
+import { useState } from 'react'
+
+export const useField = (type)=>{
+    const [value,setValue] = useState("")
+
+    const onChange = (event) =>{
+        setValue(event.target.value)
+    }
+
+    return {
+        type,
+        value,
+        onChange
+    }
+}
+
