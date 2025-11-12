ee00b576c1001f5ec79af61c4db42a6054c25956
rahul karna <rahulkarna889@gmail.com>
2025-11-12 23:15:43 +0545

    Exercise 13.6 completed

--- part13/blogs/controllers/blogController.js
+++ part13/blogs/controllers/blogController.js
@@ -27,10 +27,27 @@ blogRouter.post("/", async (req, res) => {
   }
 });
 
+blogRouter.put("/:id",async (req,res)=>{
+    try{
+        const id = req.params.id
+        const {likes} = req.body
+        const blog = await Blog.findByPk(id)
+        if(!blog){
+            res.status(404).json({error: "unable to find the blog"})
+        }
+        blog.likes = likes
+        await blog.save()
+        res.status(200).json({msg: "successfully changed likes"})
+    }
+    catch(error){
+        res.status(500).json({error: "unable to modify changes"})
+    }
+})
+
 blogRouter.delete("/:id", async (req, res) => {
   try {
     const id = req.params.id;
-    const blog = Blog.findByPk(id);
+    const blog = await Blog.findByPk(id);
     if (!blog) {
       res.status(404).json({ error: "Unable to find the blog" });
     }
