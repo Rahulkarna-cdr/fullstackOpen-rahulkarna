8a26c919816655d1a39565807ecbfedf1ea803bc
rahul karna <rahulkarna889@gmail.com>
2025-11-12 22:57:44 +0545

    Exercise 13.5 Completed

---
+++ part13/blogs/controllers/blogController.js
@@ -0,0 +1,45 @@
+const blogRouter = require("express").Router();
+
+const Blog = require("../models/blog");
+
+blogRouter.get("/", async (req, res) => {
+  const blogs = await Blog.findAll();
+  res.status(200).json(blogs);
+});
+
+blogRouter.post("/", async (req, res) => {
+  try {
+    const { author, url, title, likes } = req.body;
+    if (!url || !title) {
+      res.status(400).json({ error: "url and title are required" });
+    }
+    const blog = Blog.build({
+      author,
+      url,
+      title,
+      likes: likes || 0,
+    });
+
+    await blog.save();
+    res.status(201).json({ msg: "blog created successfully", blog });
+  } catch (error) {
+    res.status(500).json({ error: "something went wrong" });
+  }
+});
+
+blogRouter.delete("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const blog = Blog.findByPk(id);
+    if (!blog) {
+      res.status(404).json({ error: "Unable to find the blog" });
+    }
+    await Blog.destroy({ where: { id } });
+
+    res.status(200).json({ msg: "successfully deleted the blog" });
+  } catch (error) {
+    res.status(500).json({ error: "Unable to delete the blog" });
+  }
+});
+
+module.exports = blogRouter;
--- part13/blogs/index.js
+++ part13/blogs/index.js
@@ -1,95 +1,20 @@
-require("dotenv").config();
-const { Sequelize, Model, DataTypes } = require("sequelize");
-
 const express = require("express");
 const app = express();
+const {PORT} = require('./utils/config')
+const { connectToDatabase } = require('./utils/db')
+
+const blogRouter = require("./controllers/blogController")
 
 app.use(express.json());
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
-});
+app.use("/api/blogs", blogRouter)
 
-class Blog extends Model {}
-Blog.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    author: {
-      type: DataTypes.TEXT,
-    },
-    url: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    title: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    likes: {
-      type: DataTypes.INTEGER,
-      defaultValue: 0,
-    },
-  },
-  {
-    sequelize,
-    underscored: true,
-    timestamps: false,
-    modelName: "blog",
-  }
-);
 
-Blog.sync()
+const start = async () => {
+  await connectToDatabase()
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
+}
 
-app.get("/api/blogs", async (req, res) => {
-  const blogs = await Blog.findAll();
-  res.status(200).json(blogs);
-});
-
-app.post("/api/blogs", async (req, res) => {
-  try {
-    const { author, url, title, likes } = req.body;
-    if (!url || !title) {
-      res.status(400).json({ error: "url and title are required" });
-    }
-    const blog = Blog.build({
-      author,
-      url,
-      title,
-      likes: likes || 0,
-    });
-
-    await blog.save();
-    res.status(201).json({ msg: "blog created successfully", blog });
-  } catch (error) {
-    res.status(500).json({ error: "something went wrong" });
-  }
-});
-
-app.delete("/api/blogs/:id", async (req,res)=>{
-    try{
-        const id = req.params.id
-        const blog = Blog.findByPk(id)
-        if(!blog){
-            res.status(404).json({error: "Unable to find the blog"})
-        }
-        await Blog.destroy({where: {id}})
-
-        res.status(200).json({msg: "successfully deleted the blog"})
-    }
-    catch(error){
-        res.status(500).json({error: "Unable to delete the blog"})
-    }
-})
-
-app.listen(process.env.PORT, () => {
-  console.log(`server is running at PORT ${process.env.PORT}`);
-});
+start()
