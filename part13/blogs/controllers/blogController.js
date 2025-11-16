e717e914f3c2f28d4f96a03070c9d408eb722f48
rahul karna <rahulkarna889@gmail.com>
2025-11-16 20:58:40 +0545

    Fixed some Errors

--- part13/blogs/controllers/blogController.js
+++ part13/blogs/controllers/blogController.js
@@ -11,13 +11,14 @@ blogRouter.get("/", async (req, res) => {
 blogRouter.post("/",tokenExtractor, async (req, res) => {
     const { author, url, title, likes } = req.body;
     if (!url || !title) {
-      res.status(400).json({ error: "url and title are required" });
+      return res.status(400).json({ error: "url and title are required" });
     }
     const blog = Blog.build({
       author,
       url,
       title,
       likes: likes || 0,
+      userId: req.user.id
     });
 
     await blog.save();
@@ -29,7 +30,7 @@ blogRouter.put("/:id",async (req,res)=>{
         const {likes} = req.body
         const blog = await Blog.findByPk(id)
         if(!blog){
-            res.status(404).json({error: "unable to find the blog"})
+            return res.status(404).json({error: "unable to find the blog"})
         }
         blog.likes = likes
         await blog.save()
@@ -39,10 +40,14 @@ blogRouter.put("/:id",async (req,res)=>{
 blogRouter.delete("/:id",tokenExtractor, async (req, res) => {
     const id = req.params.id;
     const blog = await Blog.findByPk(id);
+
     if (!blog) {
-      res.status(404).json({ error: "Unable to find the blog" });
+      return res.status(404).json({ error: "Unable to find the blog" });
     }
-    await Blog.destroy({ where: { id } });
+    if(blog.userId!==req.user.id){
+      return res.status(403).json({error:"only own blogs can be deleted"})
+    }
+    await Blog.destroy();
 
     res.status(200).json({ msg: "successfully deleted the blog" });
 });
